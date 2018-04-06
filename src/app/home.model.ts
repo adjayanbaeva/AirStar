c9ac88663437c2ccd47f216adbd989d8d55cc099
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2018-04-06 16:18:28 -0700

    add models

---
+++ src/app/home.model.ts
@@ -0,0 +1,4 @@
+
+export class Home {
+  constructor(public imageUrl: string, public name: string, public address: string, public description: string, public amenities: string, public rules: string, public cancellation: string) { }
+}
--- src/app/homelist/homelist.component.ts
+++ src/app/homelist/homelist.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Home } from '../home.model';
 
 @Component({
   selector: 'app-homelist',
   templateUrl: './homelist.component.html',
   styleUrls: ['./homelist.component.css']
 })
-export class HomelistComponent implements OnInit {
+export class HomelistComponent {
+  homes: Home[] = [
+    new Home ("img", "Apartment", "1432 Downtown Seattle", "Beautiful apartment, within walking distance of parks, bars, museums and downtown.", "Kitchen, Wifi, Washer", "no smoking, no parties or events", "Cancel up to 5 days before check in and get a full refund (minus service fees). Cancel within 5 days of your trip" ),
+    new Home ("img", "Studio", "64829 Capitol Hill, Seattle", "Beautiful studio, within walking distance of parks, bars, museums and downtown.", "Kitchen, Wifi, Washer", "no pets", "Cancel up to 2 days before check in and get a full refund (minus service fees). Cancel within 5 days of your trip")
+  ]
+}
 
-  constructor() { }
+// implements OnInit {
 
-  ngOnInit() {
-  }
+  // constructor() { }
+  //
+  // ngOnInit() {
+  // }
 
 }
