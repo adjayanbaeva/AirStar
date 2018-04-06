3461cd6df44244c2f2ea62805c2d57fbbedf80ff
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2018-04-06 15:45:59 -0700

    add homelist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { AboutComponent } from './about/about.component';
+import { HomelistComponent } from './homelist/homelist.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomelistComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,17 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent }   from './about/about.component';
+import { HomelistComponent }   from './homelist/homelist.component';
 
 const appRoutes: Routes = [
 {
   path: 'about',
   component: AboutComponent
-}];
+},
+{
+    path: 'homelist',
+    component: HomelistComponent
+  }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
