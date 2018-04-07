e0d9d5eddf28d18c7946d4fc920bb99d4a0d7096
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2018-04-06 19:08:22 -0700

    create project in database and add records to the database

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyB4O974M18rZQWwq5qUOkQB8zkPNm8NVYI",
+    authDomain: "airstar-47249.firebaseapp.com",
+    databaseURL: "https://airstar-47249.firebaseio.com",
+    projectId: "airstar-47249",
+    storageBucket: "",
+    messagingSenderId: "1000525960832"
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { AboutComponent } from './about/about.component';
 import { HomelistComponent } from './homelist/homelist.component';
 import { HomeDetailComponent } from './home-detail/home-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 
 @NgModule({
@@ -16,7 +29,11 @@ import { HomeDetailComponent } from './home-detail/home-detail.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    FormsModule,
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
 
   ],
   providers: [],
